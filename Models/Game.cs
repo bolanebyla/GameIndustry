48a564b7d00cb7456db03049bde0121dcdf05453
Алексей <alexleskov2000@gmail.com>
2021-04-11 17:15:27 +0800

    Написать модель для игр

---
+++ Models/Game.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GameIndustry.Models
+{
+	public class Game
+	{
+		public string Name { get; set; }
+		public string Ganre { get; set; }
+		public DateTime ReleaseDate { get; set; }
+	}
+}
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,3 @@
-using GameIndustry.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -28,7 +27,6 @@ namespace GameIndustry
 		{
 			services.AddRazorPages();
 			services.AddServerSideBlazor();
-			services.AddSingleton<WeatherForecastService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
